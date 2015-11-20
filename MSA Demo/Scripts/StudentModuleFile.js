c349c3d9974ab0d507333d5cdaba86c4e2d40bdc
Jens Steinigen <jens@steinigen.co.nz>
2015-11-21 10:05:27 +1300

    first day

---
+++ MSA Demo/Scripts/StudentModuleFile.js
@@ -0,0 +1,18 @@
+﻿var StudentModule = (function () {
+    //Only functions returned from this closure is exposed and can be accessed
+    return {
+        getStudents: function (callback) {
+            //Code to make API calls goes here
+            console.log("Called from StudentModule");
+            //accessing JQuery for API
+            $.ajax({
+                type: "GET",
+                dataType: "jsonp",
+                url: "http://api.uinames.com/?amount=25",
+                success: function (data) {
+                    callback(data);
+                }
+            });
+        }
+    }
+}());
---
+++ MSA Demo/Scripts/index.js
@@ -0,0 +1,36 @@
+﻿// This event triggers on page load
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM loaded and ready to go!");
+    //calls the below define funtion loadStudents()
+    loadStudents();
+});
+function loadStudents() {
+    //call method getStudents() defined in variable StudentModule in the StudentModuleFile.js file 
+    StudentModule.getStudents(setupStudentsTable);
+    //this wouldn't work because variable StudentModule doesn't return a privateThingy function()
+    console.log(StudentModule.privateThingy);
+}
+function setupStudentsTable(studentsList) {
+    var studentTable = document.getElementById("studentListID");
+    //console.log(studentsList);
+
+    for (i = 0; i < studentsList.length; i++) {
+        var row = document.createElement("tr");
+
+        var firstNameCol = document.createElement("td");
+        firstNameCol.innerHTML = studentsList[i].name;
+        row.appendChild(firstNameCol);
+
+        var lastNameCol = document.createElement("td");
+        lastNameCol.innerHTML = studentsList[i].surname
+        row.appendChild(lastNameCol);
+
+        var countryCol = document.createElement("td");
+        countryCol.innerHTML = studentsList[i].country
+        row.appendChild(countryCol);
+
+        studentTable.appendChild(row);
+    }
+
+
+}
