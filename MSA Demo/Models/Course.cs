969de549950237c2eb78e05460049f0d348d39aa
Jens Steinigen <jens@steinigen.co.nz>
2015-11-21 12:32:16 +1300

    After API part1

---
+++ MSA Demo/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSA_Demo.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
